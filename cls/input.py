f3950332b93e344e4ad8af35d731b67cfd90986a
tjacek <tjacek@student.agh.edu.pl>
2015-10-03 21:03:43 +0200

    load data correctly

--- cls/input.py
+++ cls/input.py
@@ -7,24 +7,28 @@ class LabeledImages(object):
         self.X=images
         self.y=labels
         self.n_cat=n_cat
+        self.size=self.X.shape[0]
+        self.image_shape=self.X[0].shape
 
-def read_images(path):
+    #def get_image_shape(self):
+    #    return self.X[0].shape
+
+def load_images(path):
     cat_dirs=utils.get_dirs(path)
     n_cat=len(cat_dirs)
     images=[]
     labels=[]
     for label,category in enumerate(cat_dirs):
         cat_path=path+category
-        img_names=utils.get_dirs(cat_path)
-        img_paths=utils.append_path(cat_path,img_names)
-        for img_path in img_paths:
-            img=read_images(img_path)
+        img_names=utils.get_paths(cat_path)
+        for img in utils.read_images(img_names):
             images.append(img)
             labels.append(label)
-        images=np.array(images)
-        labels=np.array(labels)
+    images=np.array(images)
+    labels=np.array(labels)
     return LabeledImages(images,labels,n_cat)
  
 if __name__ == "__main__":
     path="/home/user/cf/conv_frames/cls/images/"
-    read_images(path)
+    images=load_images(path)
+    print(images.image_shape)
