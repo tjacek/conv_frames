35d95d4c6ce95fb9df0a89a62be65c524742e166
tjacek <tjacek@student.agh.edu.pl>
2015-10-05 21:30:17 +0200

    convolutional net

--- cls/conv_net.py
+++ cls/conv_net.py
@@ -1,13 +1,9 @@
-import ml_tools
+import load,ml_tools,nn,logit
 import numpy as np
 import theano
 import theano.tensor as T
 from theano.tensor.signal import downsample
 from theano.tensor.nnet import conv
-
-from logistic_sgd import LogisticRegression, load_data
-from mlp import HiddenLayer
-
 
 class LeNetConvPoolLayer(object):
 
@@ -26,10 +22,10 @@ class LeNetConvPoolLayer(object):
         self.params = [self.W, self.b]
 
     def __init_W(self,fan_in,fan_out,filter_shape):
-        init_value=self.rng.uniform(fan_in,fan_out,w_dim)
+        init_value=self.rng.uniform(fan_in,fan_out,filter_shape)
         self.W = theano.shared(
-            numpy.asarray(
-                rng.uniform(low=-W_bound, high=W_bound, size=filter_shape),
+            np.asarray(
+                init_value,
                 dtype=theano.config.floatX
             ),
             borrow=True
@@ -55,193 +51,73 @@ class LeNetConvPoolLayer(object):
 
         self.output = T.tanh(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
 
-def evaluate_lenet5(learning_rate=0.1, n_epochs=200,
-                    nkerns=[20, 50], batch_size=500):
+def build_model(dataset,model_params):
+    batch_size=model_params['batch_size']
+    learning_rate=model_params['learning_rate']
+    nkerns=model_params['nkerns']
 
-    ######################
-    # BUILD ACTUAL MODEL #
-    ######################
-    print '... building the model'
+    symb_vars=ml_tools.InputVariables()
+    image_dim=dataset.image_shape
+    input0_shape=(batch_size,1,image_dim[0],image_dim[1])
+    layer0_input = symb_vars.x.reshape(input0_shape)
 
-    # Reshape matrix of rasterized images of shape (batch_size, 28 * 28)
-    # to a 4D tensor, compatible with our LeNetConvPoolLayer
-    # (28, 28) is the size of MNIST images.
-    layer0_input = x.reshape((batch_size, 1, 28, 28))
-
-    # Construct the first convolutional pooling layer:
-    # filtering reduces the image size to (28-5+1 , 28-5+1) = (24, 24)
-    # maxpooling reduces this further to (24/2, 24/2) = (12, 12)
-    # 4D output tensor is thus of shape (batch_size, nkerns[0], 12, 12)
     layer0 = LeNetConvPoolLayer(
-        rng,
-        input=layer0_input,
-        image_shape=(batch_size, 1, 28, 28),
+        input_data=layer0_input,
+        image_shape=input0_shape,
         filter_shape=(nkerns[0], 1, 5, 5),
         poolsize=(2, 2)
     )
 
-    # Construct the second convolutional pooling layer
-    # filtering reduces the image size to (12-5+1, 12-5+1) = (8, 8)
-    # maxpooling reduces this further to (8/2, 8/2) = (4, 4)
-    # 4D output tensor is thus of shape (batch_size, nkerns[1], 4, 4)
+    input1_shape=(batch_size,nkerns[0],38, 18)
     layer1 = LeNetConvPoolLayer(
-        rng,
-        input=layer0.output,
-        image_shape=(batch_size, nkerns[0], 12, 12),
+        input_data=layer0.output,
+        image_shape=input1_shape,
         filter_shape=(nkerns[1], nkerns[0], 5, 5),
         poolsize=(2, 2)
     )
 
-    # the HiddenLayer being fully-connected, it operates on 2D matrices of
-    # shape (batch_size, num_pixels) (i.e matrix of rasterized images).
-    # This will generate a matrix of shape (batch_size, nkerns[1] * 4 * 4),
-    # or (500, 50 * 4 * 4) = (500, 800) with the default values.
     layer2_input = layer1.output.flatten(2)
-
-    # construct a fully-connected sigmoidal layer
-    layer2 = HiddenLayer(
-        rng,
-        input=layer2_input,
-        n_in=nkerns[1] * 4 * 4,
+    print(nkerns)
+    layer2 = nn.HiddenLayer(
+        input_data=layer2_input,
+        n_in=nkerns[1] * (59*2 +1),
         n_out=500,
         activation=T.tanh
     )
 
-    # classify the values of the fully-connected sigmoidal layer
-    layer3 = LogisticRegression(input=layer2.output, n_in=500, n_out=10)
+    layer3 = logit.LogisticRegression(input_data=layer2.output, 
+                           n_in=500, n_out=dataset.n_cats)
 
-    # the cost we minimize during training is the NLL of the model
-    cost = layer3.negative_log_likelihood(y)
+    cost = layer3.negative_log_likelihood(symb_vars.y)
 
-    # create a function to compute the mistakes that are made by the model
     test_model = theano.function(
-        [index],
-        layer3.errors(y),
-        givens={
-            x: test_set_x[index * batch_size: (index + 1) * batch_size],
-            y: test_set_y[index * batch_size: (index + 1) * batch_size]
-        }
+        [symb_vars.x, symb_vars.y],
+        layer3.errors(symb_vars.y),
     )
 
-    validate_model = theano.function(
-        [index],
-        layer3.errors(y),
-        givens={
-            x: valid_set_x[index * batch_size: (index + 1) * batch_size],
-            y: valid_set_y[index * batch_size: (index + 1) * batch_size]
-        }
-    )
-
-    # create a list of all model parameters to be fit by gradient descent
-    params = layer3.params + layer2.params + layer1.params + layer0.params
-
-    # create a list of gradients for all model parameters
+    params = layer3.params + layer2.params + layer1.params + layer0.params 
     grads = T.grad(cost, params)
 
-    # train_model is a function that updates the model parameters by
-    # SGD Since this model has many parameters, it would be tedious to
-    # manually create an update rule for each model parameter. We thus
-    # create the updates list by automatically looping over all
-    # (params[i], grads[i]) pairs.
     updates = [
         (param_i, param_i - learning_rate * grad_i)
         for param_i, grad_i in zip(params, grads)
     ]
 
     train_model = theano.function(
-        [index],
+        [symb_vars.x, symb_vars.y],
         cost,
-        updates=updates,
-        givens={
-            x: train_set_x[index * batch_size: (index + 1) * batch_size],
-            y: train_set_y[index * batch_size: (index + 1) * batch_size]
-        }
+        updates=updates
     )
-    # end-snippet-1
+    return layer3,train_model
 
-    ###############
-    # TRAIN MODEL #
-    ###############
-    print '... training'
-    # early-stopping parameters
-    patience = 10000  # look as this many examples regardless
-    patience_increase = 2  # wait this much longer when a new best is
-                           # found
-    improvement_threshold = 0.995  # a relative improvement of this much is
-                                   # considered significant
-    validation_frequency = min(n_train_batches, patience / 2)
-                                  # go through this many
-                                  # minibatche before checking the network
-                                  # on the validation set; in this case we
-                                  # check every epoch
-
-    best_validation_loss = numpy.inf
-    best_iter = 0
-    test_score = 0.
-    start_time = timeit.default_timer()
-
-    epoch = 0
-    done_looping = False
-
-    while (epoch < n_epochs) and (not done_looping):
-        epoch = epoch + 1
-        for minibatch_index in xrange(n_train_batches):
-
-            iter = (epoch - 1) * n_train_batches + minibatch_index
-
-            if iter % 100 == 0:
-                print 'training @ iter = ', iter
-            cost_ij = train_model(minibatch_index)
-
-            if (iter + 1) % validation_frequency == 0:
-
-                # compute zero-one loss on validation set
-                validation_losses = [validate_model(i) for i
-                                     in xrange(n_valid_batches)]
-                this_validation_loss = numpy.mean(validation_losses)
-                print('epoch %i, minibatch %i/%i, validation error %f %%' %
-                      (epoch, minibatch_index + 1, n_train_batches,
-                       this_validation_loss * 100.))
-
-                # if we got the best validation score until now
-                if this_validation_loss < best_validation_loss:
-
-                    #improve patience if loss improvement is good enough
-                    if this_validation_loss < best_validation_loss *  \
-                       improvement_threshold:
-                        patience = max(patience, iter * patience_increase)
-
-                    # save best validation score and iteration number
-                    best_validation_loss = this_validation_loss
-                    best_iter = iter
-
-                    # test it on the test set
-                    test_losses = [
-                        test_model(i)
-                        for i in xrange(n_test_batches)
-                    ]
-                    test_score = numpy.mean(test_losses)
-                    print(('     epoch %i, minibatch %i/%i, test error of '
-                           'best model %f %%') %
-                          (epoch, minibatch_index + 1, n_train_batches,
-                           test_score * 100.))
-
-            if patience <= iter:
-                done_looping = True
-                break
-
-    end_time = timeit.default_timer()
-    print('Optimization complete.')
-    print('Best validation score of %f %% obtained at iteration %i, '
-          'with test performance %f %%' %
-          (best_validation_loss * 100., best_iter + 1, test_score * 100.))
-    print >> sys.stderr, ('The code for file ' +
-                          os.path.split(__file__)[1] +
-                          ' ran for %.2fm' % ((end_time - start_time) / 60.))
+def get_default_params(learning_rate=0.13):
+    params={'learning_rate': learning_rate,
+            'nkerns':[20, 50],
+            'batch_size':1}
+    return params
 
 if __name__ == '__main__':
-    evaluate_lenet5()
-
-
-def experiment(state, channel):
-    evaluate_lenet5(state.learning_rate, dataset=state.dataset)
+    path="/home/user/cf/conv_frames/cls/images/"
+    dataset=load.get_images(path)
+    params=get_default_params()
+    cls=ml_tools.learning_iter(dataset,build_model,params,n_epochs=100)
