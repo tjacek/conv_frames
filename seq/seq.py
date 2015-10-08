889011f449db2859dc7f882abd7be348d74451d6
tjacek <tjacek@student.agh.edu.pl>
2015-10-08 22:44:46 +0200

    save seqs

--- seq/seq.py
+++ seq/seq.py
@@ -8,7 +8,13 @@ class Instances(object):
         self.cats=cats
         self.persons=persons
 
-def parse_dataset(path):
+    def get_instance(self,i):
+        return self.seqs[i],self.cats[i],self.persons[i]
+
+    def to_tuple(self):
+        return [self.get_instance(i) for i in range(self.size)]
+
+def parse_instances(path):
     lines=utils.read_file(path)
     instances=map(parse_instance,lines)
     seqs=[inst[0] for inst in instances]
@@ -16,13 +22,23 @@ def parse_dataset(path):
     persons=[inst[2] for inst in instances]
     return Instances(seqs,cats,persons)
 
+def save_instances(out_path,instances):
+    lines=instances.to_tuple()
+    txt=utils.array_to_txt(lines)
+    utils.save_string(out_path,txt)
+
 def parse_instance(raw_instance):
     raw=raw_instance.split('$')
-    seq=raw[0]#get_frames(raw[0])
+    seq=raw[0]
     category=int(raw[1])
     person=int(raw[2])
     return seq,category,person
 
+def seq_to_string(seq):
+    return seq[0] + "$" + seq[1] +"$" +seq[2]
+
+
 if __name__ == "__main__":
     path="/home/user/cf/seqs/dataset.seq"
-    print(parse_dataset(path))
+    inst=parse_instances(path)
+    save_instances(path.replace(".seq","_.seq"),inst)
