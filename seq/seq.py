4d087e5a66437de4b2d9d618db76953f9a1c293f
tjacek <tjacek@student.agh.edu.pl>
2015-10-12 11:36:17 +0200

    refactore seq

--- seq/seq.py
+++ seq/seq.py
@@ -1,44 +1,50 @@
 import imp
 utils =imp.load_source("utils","/home/user/cf/conv_frames/utils.py")
 
-class Instances(object):
-    def __init__(self,seqs,cats,persons):
-        self.size=len(seqs)
-        self.seqs=seqs
-        self.cats=cats
-        self.persons=persons
+class Dataset(object):
+    def __init__(self,instances):
+        self.size=len(instances)
+        self.instances=instances
 
     def get_instance(self,i):
-        return self.seqs[i],self.cats[i],self.persons[i]
+        return self.instances[i]
 
-    def to_tuple(self):
-        return [self.get_instance(i) for i in range(self.size)]
+    def get_seq(self,i):
+        return self.instances[i].seq
 
-def parse_instances(path):
+    def get_category(self,i):
+        return self.instances[i].category
+
+    def get_person(self,i):
+        return self.instances[i].person
+
+class Instance(object):
+    def __init__(self,seq,category,person):
+        self.seq=seq
+        self.category=category
+        self.person=person 
+
+    def __str__(self):
+        cat=str(self.category)
+        person=str(self.person)
+        return str(self.seq) + "$" + cat +"$" + person
+
+def create_dataset(path):
     lines=utils.read_file(path)
     instances=map(parse_instance,lines)
-    seqs=[inst[0] for inst in instances]
-    cats=[inst[1] for inst in instances]
-    persons=[inst[2] for inst in instances]
-    return Instances(seqs,cats,persons)
+    return Dataset(instances)
 
-def save_instances(out_path,instances):
-    lines=instances.to_tuple()
-    txt=utils.array_to_txt(lines)
-    utils.save_string(out_path,txt)
-
+def save_instances(out_path,dataset):
+    utils.to_txt_file(out_path,dataset.instances)
+    
 def parse_instance(raw_instance):
     raw=raw_instance.split('$')
     seq=raw[0]
     category=int(raw[1])
     person=int(raw[2])
-    return seq,category,person
-
-def seq_to_string(seq):
-    return seq[0] + "$" + seq[1] +"$" +seq[2]
-
+    return Instance(seq,category,person)
 
 if __name__ == "__main__":
     path="/home/user/cf/seqs/dataset.seq"
-    inst=parse_instances(path)
-    save_instances(path.replace(".seq","_.seq"),inst)
+    dataset=create_dataset(path)
+    save_instances(path.replace(".seq","_.seq"),dataset)
