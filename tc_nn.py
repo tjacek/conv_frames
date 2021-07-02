14cca55431a104a924985dcfc0f93eb7522ea528
tjacek <tjacek@agh.edu.pl>
2021-07-02 22:27:49 +0200

    TC_NN class

--- tc_nn.py
+++ tc_nn.py
@@ -11,27 +11,32 @@ from keras.models import load_model
 import tensorflow.keras.losses
 import data.imgs,data.feats
 
-def make_tcn(params):
-    inputs = Input(shape=(params['seq_len'],*params['dims'],1))
-    x = Lambda(lambda y: K.reshape(y, (-1, *params['dims'], 1)))(inputs)
-    x = Conv2D(16, 5)(x)
-    x = MaxPool2D()(x)
-    x = Conv2D(16, 5)(x)
-    x = MaxPool2D()(x)
-    num_features_cnn = np.prod(K.int_shape(x)[1:])
-    x = Lambda(lambda y: K.reshape(y, (-1, params['seq_len'], num_features_cnn)))(x)
-    x = TCN(2*params['n_cats'],name="hidden")(x)
-    x = Dense(params['n_cats'], activation='sigmoid')(x)
-    model = Model(inputs=[inputs], outputs=[x])
-    model.summary()
-    tcn_full_summary(model, expand_residual_blocks=False)
-    model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
-    return model
-#    tcn_layer = TCN(input_shape=(params["ts_len"], params["n_feats"]),name="tcn_layer")
-#    m = Sequential([tcn_layer,Dense(units=params['n_cats'],activation='softmax')])
-#    m.compile(optimizer='adam', loss=tensorflow.keras.losses.categorical_crossentropy)
-#    tcn_full_summary(m, expand_residual_blocks=False)
-#    return m
+class TC_NN(object):
+    def __init__(self):
+        self.n_kern=[64,64,64]
+        self.kern_size=[(5,5),(5,5),(5,5)]
+        self.pool_size=[(2,2),(2,2),(2,2)]	
+
+    def __call__(self,params):
+        inputs = Input(shape=(params['seq_len'],*params['dims'],1))
+        x = Lambda(lambda y: K.reshape(y, (-1, *params['dims'], 1)))(inputs)
+        for i,n_kern_i in enumerate(self.n_kern):
+            x=Conv2D(n_kern_i, kernel_size=self.kern_size[i])(x)
+            x=MaxPool2D(pool_size=self.pool_size[i])(x)
+#        x = Conv2D(16, 5)(x)
+#        x = MaxPool2D()(x)
+#        x = Conv2D(16, 5)(x)
+#        x = MaxPool2D()(x)
+        num_features_cnn = np.prod(K.int_shape(x)[1:])
+        x = Lambda(lambda y: K.reshape(y, (-1, params['seq_len'], num_features_cnn)))(x)
+        x = TCN(2*params['n_cats'],name="hidden")(x)
+        x = Dense(params['n_cats'], activation='sigmoid')(x)
+        model = Model(inputs=[inputs], outputs=[x])
+#       model.summary()
+        tcn_full_summary(model, expand_residual_blocks=False)
+        model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+        return model
+
 
 def single_train(in_path,out_path=None,seq_size=20,n_epochs=5):
     frame_seq=data.imgs.read_frame_seqs(in_path,n_split=1)
@@ -41,6 +46,7 @@ def single_train(in_path,out_path=None,seq_size=20,n_epochs=5):
     X,y,params=to_dataset(train)
     if("n_cats" in params ):
         y=to_one_hot(y,params["n_cats"])
+    make_tcn=TC_NN()
     model=make_tcn(params)
     model.fit(X,y,epochs=n_epochs)
     if(out_path):
@@ -51,6 +57,7 @@ def single_extract(in_path,nn_path,out_path):
     frame_seq.subsample(20)
     frame_seq.scale((64,64))
     params={'seq_len':20,'dims':(64,64),'n_cats':frame_seq.n_cats()}
+    make_tcn=TC_NN()
     model=make_tcn(params)   
 #    model=make_tcn(params)
     model.load_weights(nn_path)
@@ -77,5 +84,5 @@ def to_one_hot(y,n_cats=20):
     return one_hot
 
 in_path="../MSR/frames"
-single_train(in_path,"test",n_epochs=200)
+#single_train(in_path,"test",n_epochs=100)
 single_extract(in_path,"test","feats")
