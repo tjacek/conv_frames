98a238913b8cc838c0175a4087e92d23e6584825
tjacek <tjacek@agh.edu.pl>
2021-07-07 21:34:27 +0200

    fix bug in student

--- student.py
+++ student.py
@@ -1,15 +1,17 @@
 import data.feats
 import tc_nn,learn,files
 
-def train_student(frame_path,teacher_path,out_path,n_epochs=5):
+def train_student(frame_path,teacher_path,out_path,n_epochs=100):
     files.make_dir(out_path)
     read=tc_nn.get_read(seq_len=20,dim=(64,64))
     frame_seq=read(frame_path)
+    frame_seq=frame_seq.split()[0]
     make_tcn=tc_nn.TC_NN(loss='mean_squared_error')
     params={'seq_len':frame_seq.min_len(),'dims':frame_seq.dims(),
                 'n_cats':frame_seq.n_cats() * frame_seq.n_cats()}
     model=make_tcn(params)
     teacher_feat=data.feats.read_feats(teacher_path)
+    teacher_feat=teacher_feat.split()[0]
     y=teacher_feat.to_dataset()[0]
     X=frame_seq.to_dataset()[0]
     model.fit(X,y,epochs=n_epochs)
@@ -24,13 +26,11 @@ def extract_student(frame_path,nn_path,out_path):
     model=learn.read_model(frame_seq,"%s/nn" %nn_path,make_tcn,params)
     model.summary()
     X,y=frame_seq.to_dataset()
-#    pred_y=model.predict(X)
-#    print(pred_y.shape)
     feats=learn.get_features(frame_seq,model)
     feats.save("%s/feats"  % out_path)
 
 frame_path="../../2021_VI/raw_3DHOI/3DHOI/frames"
 teacher_path="../ml_utils/3DHOI"
 nn_path="student"
-#train_student(frame_path,teacher_path,"student")
+train_student(frame_path,teacher_path,"student")
 extract_student(frame_path,nn_path,nn_path)
