818168293d56ec5a23a9fa7db4ea7d549958ba7e
tjacek <tjacek@student.agh.edu.pl>
2021-07-21 21:53:32 +0200

    student ensemble

--- student.py
+++ student.py
@@ -1,6 +1,6 @@
 import numpy as np
 import data.feats
-import tc_nn,learn,files
+import tc_nn,learn,files,ens
 
 class TrainStudent(object):
     def __init__(self,make_tcn=None,batch_size=16):
@@ -9,7 +9,7 @@ class TrainStudent(object):
         self.make_tcn=make_tcn
         self.batch_size=batch_size
 
-    def __call__(self,frame_path,teacher_path,out_path,n_epochs=100):
+    def __call__(self,frame_path,teacher_path,out_path,n_epochs=5):
         files.make_dir(out_path)
         frame_seq,teacher_feat,params=read_data(frame_path,teacher_path)
         model=self.make_tcn(params)
@@ -26,7 +26,7 @@ class ExtractStudent(object):
         self.make_tcn=make_tcn
         self.read=tc_nn.get_read(seq_len=20,dim=(64,64))
 
-    def __call__(self,mframe_path,nn_path,out_path,n_cats):
+    def __call__(self,frame_path,nn_path,out_path,n_cats):
         frame_seq=self.read(frame_path)
         params={'seq_len':frame_seq.min_len(),'dims':frame_seq.dims(),
                 'n_cats':n_cats}
@@ -34,7 +34,7 @@ class ExtractStudent(object):
         model.summary()
         X,y=frame_seq.to_dataset()
         feats=learn.get_features(frame_seq,model)
-        feats.save("%s/feats"  % out_path)
+        feats.save(out_path)#"%s/feats"  % out_path)
 
 def read_data(frame_path,teacher_path):
     read=tc_nn.get_read(seq_len=20,dim=(64,64))
@@ -62,17 +62,20 @@ def single_student(frame_path,teacher_path,nn_path,n_epochs=100):
     np.set_printoptions(threshold=n_cats)
     extract(frame_path,nn_path,nn_path,n_cats)
 
-def ens_student():
-    train=learn.Train(to_dataset,make_nn,read,batch_size=16)
-    extract=learn.Extract(make_nn,read,name="hidden")
-    funcs=[[train,["feats","nn","n_epochs"]],
-           [extract,["feats","nn","simple_feats"]]]
-    dir_names=["nn","simple_feats"]
-    ensemble=ens.EnsTransform(funcs,dir_names,"feats")
+def ens_student(frame_path,student_path,out_path,n_epochs=5):
+    make_tcn=tc_nn.TC_NN(n_hidden=100,batch=True,loss='mean_squared_error')
+    train,extract=TrainStudent(make_tcn),ExtractStudent(make_tcn)
+    funcs=[[train,[ "frame","teacher","nn","n_epochs"]],
+           [extract,[ "frame","nn","feats","n_cats"]]]
+    dir_names=["nn","feats"]
+    ensemble=ens.EnsTransform(funcs,dir_names,"teacher")
     files.make_dir(out_path)
-    ensemble(input_paths,out_path, arg_dict={"n_epochs":n_epochs})
+    input_paths=files.top_files(student_path)
+    args={"n_epochs":n_epochs,"n_cats":100,"frame":frame_path}
+    ensemble(input_paths,out_path, arg_dict=args)
 
 frame_path="../3DHOI/frames"
 teacher_path="../ml_utils/3DHOI_simple"
 nn_path="student_simple_nobatch"
-single_student(frame_path,teacher_path,nn_path,n_epochs=100)
+#single_student(frame_path,teacher_path,nn_path,n_epochs=100)
+ens_student(frame_path,"test/simple_feats","student_ens",n_epochs=100)
