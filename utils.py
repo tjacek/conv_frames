48c2fc6447927395a94f3898ea97bbdcadb1100f
tjacek <tjacek@student.agh.edu.pl>
2021-07-27 17:57:17 +0200

    validate regression

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 from collections import defaultdict
 import data.feats
 
@@ -17,7 +18,23 @@ def name_map(res_feats):
 		name_id[name_i.subname(3)].append(name_i)
 	return name_id
 
-true_path="../3DHOI/1D_CNN/feats"
-pred_path="student/ae_30_200_agum/feats"
-out_path="student/ae_30_200_agum/res"
-residuals(true_path,pred_path,out_path)
+def fill_agum( raw_feats,agum_feats,data_type=data.feats.Feats):
+    name_id=name_map(agum_feats)
+    full_feats=data.feats.Feats()
+    for name_i,name_list_i in name_id.items():
+        for name_j in name_list_i:
+            full_feats[name_j]=raw_feats[name_i]
+    return full_feats
+
+def validate_regresion(true_path,pred_path):
+    true_feats=data.feats.read_feats(true_path).split()[1]
+    pred_feats=data.feats.read_feats(pred_path).split()[1]
+    error=[ (true_feats[name_i]-pred_feats[name_i])**2  
+                for name_i in true_feats.keys()]
+    mse=np.mean(error)
+    print(mse)
+
+if __name__ == "__main__":
+    true_path="smooth3/simple/feats"
+    pred_path="smooth3/simple/base_120/feats"
+    validate_regresion(true_path,pred_path)
