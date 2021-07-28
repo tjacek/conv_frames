614f32ba2b99daefcaca2fdedc70c4469727c443
tjacek <tjacek@student.agh.edu.pl>
2021-07-29 01:49:26 +0200

    resample

--- preproc/paths.py
+++ preproc/paths.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append("..")
+import numpy as np
 import files
 
 class Paths(dict):
@@ -15,13 +16,22 @@ class Paths(dict):
     def min_len(self):
         return min(self.seqs_len())	
 
+    def supsample(self,size=100):
+        for name_i,frames_i in self.items():
+            size_i=len(frames_i)
+            replace_i= (size_i<size)  
+            indexes=np.random.choice(range(size_i),size=size,replace=replace_i)
+            indexes.sort()
+            self[name_i]=[frames_i[j] for j in indexes ]
+
 def read_paths(in_path):
 	paths=Paths()
 	for dir_i in files.top_files(in_path):
 	    paths[dir_i]=files.top_files(dir_i)
 	return paths	
 
-in_path="../../../2021_VI/raw_3DHOI/3DHOI/frames"
+in_path="../../../Downloads/AA/depth/depth_only"
 paths=read_paths(in_path)
-print(paths.max_len())
-print(paths.min_len())
+#print(paths.max_len())
+#print(paths.min_len())
+paths.supsample()
