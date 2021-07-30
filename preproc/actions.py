3a443b6a244c449d18c55cfde7824f799db22cda
tjacek <tjacek@student.agh.edu.pl>
2021-07-30 15:27:30 +0200

    diff action

--- preproc/actions.py
+++ preproc/actions.py
@@ -4,9 +4,14 @@ import numpy as np
 import data.actions
 
 def make_actions(in_path,out_path):
-    fun= lambda x: np.mean(x,axis=0)
-    data.actions.get_actions(in_path,fun,out_path,dims=None)	
+#    fun= lambda x: np.mean(x,axis=0)
+    data.actions.get_actions(in_path,diff,out_path,dims=None)	
 
-in_path="../../../Downloads/AA/depth/depth_sampled"
+def diff(frames):
+    diff=[ np.abs(frames[i-1]-frames[i]) 
+                for i in range(1,len(frames))]	
+    return  np.mean(diff,axis=0)
+
+in_path="../../../Downloads/AA/depth/rename"
 out_path="../../../Downloads/AA/depth/actions"
 make_actions(in_path,out_path)
