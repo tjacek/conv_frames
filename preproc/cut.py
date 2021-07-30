71654f1ba81ee249737b9886f92c143cd814f2c7
tjacek <tjacek@student.agh.edu.pl>
2021-07-31 00:16:58 +0200

    Train Dec

--- preproc/cut.py
+++ preproc/cut.py
@@ -29,6 +29,19 @@ class ActionState(object):
         with open(path_i, 'w') as f:
             json.dump(self.train_data, f)
 
+class TrainDec(object):
+    def __init__(self,train_data):
+        self.data=train_data
+
+    def __contains__(self, name_i):
+        return (np.product(self.data[name_i])!=0)
+
+    def __call__(self,name_i,img_i):
+        position_i=self.data[name_i]
+        x0,y0=position_i[0],position_i[1]
+        x1,y1=x0+position_i[2],y0+position_i[3]     
+        return img_i[x0:x1,y0:y1]
+
 def cut_rect(img_i,position):
 	if(np.product(position)==0):
 		return img_i
@@ -39,21 +52,34 @@ def cut_rect(img_i,position):
 	x0,y0=position[0],position[1]
 	x1,y1=x0+position[2],y0+position[3]
 	img_i=img_i.copy()
-	img_i[:x1,y0:y1]=0
+	img_i[x0:x1,y0:y1]=200
 	return img_i
+
+def cut_actions(in_path,train_path,out_path):
+    actions_dict=data.actions.read_actions(in_path)
+    train_data=TrainDec(read_train(train_path))
+    new_actions=data.actions.ActionImgs()
+    for name_i,action_i in actions_dict.items():
+        if(name_i in train_data):
+            new_actions[name_i]= train_data(name_i,action_i)
+    new_actions.save(out_path)
 
 def make_train(actions_dict):
     return {name_i:[0,0,0,0] for name_i in actions_dict.keys()}	
 
+def read_train(train_path):
+    return json.load(open(train_path))
+
 def make_action_state(in_path,train_path="train"):
     actions_dict=data.actions.read_actions(in_path)
     if(os.path.isfile(train_path)):
-        train_data=json.load(open(train_path))
+        train_data=read_train(train_path)#json.load(open(train_path))
     else:
         train_data=make_train(actions_dict)
     return ActionState(actions_dict,train_data,train_path)
 
 if __name__ == "__main__":
     in_path="../../../Downloads/AA/depth/actions"
-    state=make_action_state(in_path)
-    gui.gui_exp(state)
+#    state=make_action_state(in_path)
+#    gui.gui_exp(state)
+    cut_actions(in_path,"train","actions")
