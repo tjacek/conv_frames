c31326b0a86d2a3ca11fbd2b9c53b5d8295f3342
tjacek <tjacek@student.agh.edu.pl>
2021-08-02 22:56:38 +0200

    selected exp

--- preproc/paths.py
+++ preproc/paths.py
@@ -61,15 +61,30 @@ def dir_map(in_path):
 def get_name(path_i):
     return path_i.split("/")[-1]
 
+def replace(path_0,path_1):
+    name_i=get_name(path_0)
+    return "%s/%s" % (path_1,name_i)
+
+def selectd_exp(in_path,s_path,out_path):
+    raw_paths=dir_map(in_path)
+    s_paths=read_paths(s_path)
+    files.make_dir(out_path)
+    for selected_i,data_i in s_paths.items():
+        name_i=get_name(selected_i)
+        raw_i=raw_paths[name_i]
+        out_i="%s/%s" % (out_path,name_i)
+        print(out_i)
+        files.make_dir(out_i)
+        for frame_j in data_i:
+            in_j=replace(frame_j,raw_i)            
+            out_j=replace(frame_j,out_i)
+            in_j=in_j.replace(".png","")
+            print(in_j)
+            print(out_j)
+            shutil.copy(in_j,out_j)
 
 if __name__ == "__main__":
     in_path="../../3DHOI2/raw"
     s_path="../../3DHOI/short4/frames"
     out_path="../../3DHOI2/selected"
-    raw_paths=dir_map(in_path)
-    s_paths=read_paths(s_path)
-    for selected_i,data_i in s_paths.items():
-        name_i=get_name(selected_i)
-        raw_i=raw_paths[name_i]
-        print(selected_i)
-        print(raw_i)
+    selectd_exp(in_path,s_path,out_path)
