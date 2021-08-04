87f276504a60fce53ca0b51ea4b312b2faa9c72b
tjacek <tjacek@student.agh.edu.pl>
2021-08-04 21:53:53 +0200

    refactore paths

--- preproc/paths.py
+++ preproc/paths.py
@@ -8,8 +8,6 @@ class Paths(dict):
     def __init__(self, arg=[]):
         super(Paths, self).__init__(arg)
 
-#    def __getitem__(self, p):
-
     def seqs_len(self):
         return [ len(path_i) for path_i in self.values()]	
 
@@ -26,22 +24,19 @@ class Paths(dict):
             indexes=np.random.choice(range(size_i),size=size,replace=replace_i)
             indexes.sort()
             self[name_i]=[frames_i[j] for j in indexes ]
-
+    
     def save(self,out_path):
         files.make_dir(out_path)
-        for name_i,frames_i in self.items():
-            new_dir_i="%s/%s" % (out_path,name_i.split("/")[-1])
+        for dir_i,frames_i in self.items():
+            name_i=get_name(dir_i)
+            new_dir_i="%s/%s" % (out_path,name_i)
             new_dir_i=new_dir_i.replace(".._..","_")
             files.make_dir(new_dir_i)
-            print(new_dir_i)
-            print(len(frames_i))
             for j,path_j in enumerate(frames_i):
                 path_j= path_j.replace(".._..","_")
-                id_j=path_j.split("/")[-2]
-                name_j="%s/%s/%d" % (out_path,id_j,j)
-                print(name_j)
+                name_j="%s/%s/%d" % (out_path,name_i,j)
                 shutil.copy(path_j,name_j)
-
+                
 def read_paths(in_path):
 	paths=Paths()
 	for dir_i in files.top_files(in_path):
