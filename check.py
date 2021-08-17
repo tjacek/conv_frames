12c460f819f356bcea2ba7939ea57aab8e187a86
tjacek <tjacek@student.agh.edu.pl>
2021-08-18 00:02:16 +0200

    check

--- check.py
+++ check.py
@@ -23,6 +23,18 @@ def split_size(in_path):
     print("train:%d" % len(train))
     print("test:%d" % len(test))
 
-in_path="../3DHOI2/try2/frames"
-in_path="../3DHOI2/raw"
-split_size(in_path)
+def mean_value(in_path):
+    frame_seqs=data.imgs.read_frame_seqs(in_path,n_split=1)
+    cats=by_cat(frame_seqs)
+    for cat_i,names_i in cats.items():
+        print(cat_i)
+        values=[ (name_j,np.mean(frame_seqs[name_j])) 
+                    for name_j in names_i]
+        mean_i=np.mean([value_i[1] for value_i in values])
+#        print(values)
+        print(mean_i)
+
+in_path="../../2021_VII/3DHOI/frames"
+in_path="../3DHOI3/full6/frames"
+#seq_len(in_path)
+mean_value(in_path)
