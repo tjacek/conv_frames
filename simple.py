c8a13fcc0325da990136a92f005bd840710ee215
tjacek <tjacek@student.agh.edu.pl>
2021-09-05 23:49:16 +0200

    SimpleNN class

--- simple.py
+++ simple.py
@@ -6,16 +6,20 @@ from tensorflow.keras import regularizers
 from tensorflow.keras import optimizers
 import data.feats,learn,ens,files
 
-def make_nn(params):
-    model = Sequential()
-    model.add(Dense(100, input_dim=params['dims'], activation='relu',name="hidden",
-        kernel_regularizer=regularizers.l1(0.001)))
-    model.add(BatchNormalization())
-    model.add(Dense(params['n_cats'], activation='softmax'))
-    optim=optimizers.RMSprop(learning_rate=0.00001)
-    model.compile(loss='categorical_crossentropy',optimizer=optim, metrics=['accuracy'])
-    model.summary()
-    return model
+class SimpleNN(objects):
+    def __init__(self,n_hidden=100)
+        self.n_hidden=n_hidden
+        self.optim=optimizers.RMSprop(learning_rate=0.00001)
+
+    def __call__(self,params):
+        model = Sequential()
+        model.add(Dense(self.n_hidden, input_dim=params['dims'], activation='relu',name="hidden",
+            kernel_regularizer=regularizers.l1(0.001)))
+        model.add(BatchNormalization())
+        model.add(Dense(params['n_cats'], activation='softmax'))
+        model.compile(loss='categorical_crossentropy',optimizer=self.optim, metrics=['accuracy'])
+        model.summary()
+        return model
 
 def ensemble_exp(common,binary,out_path,n_epochs=5):
     input_paths=files.top_files(binary)
@@ -25,6 +29,7 @@ def ensemble_exp(common,binary,out_path,n_epochs=5):
         feats_i.norm()
         feats_i.remove_nan()
         return feats_i
+    make_nn=SimpleNN()
     train=learn.Train(to_dataset,make_nn,read,batch_size=16)
     extract=learn.Extract(make_nn,read,name="hidden")
     funcs=[[train,["feats","nn","n_epochs"]],
@@ -36,6 +41,7 @@ def ensemble_exp(common,binary,out_path,n_epochs=5):
 
 def single_exp(feat_path,nn_path,out_path,n_epochs=5):
     read=data.feats.read_feats
+    make_nn=SimpleNN()
     train=learn.Train(to_dataset,make_nn,read,batch_size=16)
     train(feat_path,nn_path,n_epochs)
     extract=learn.Extract(make_nn,read=read,name="hidden")
