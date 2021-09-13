70b132009b52c7b6e9919c8aa8ba690aa42bcc0d
tjacek <tjacek@agh.edu.pl>
2021-09-13 18:42:33 +0200

    refactor ts_sim

--- deep.py
+++ deep.py
@@ -1,17 +1,20 @@
-from keras.layers import Conv2D,Conv1D, MaxPooling1D,MaxPooling2D
-from keras.layers import Dropout,Flatten,Dense
-from keras import regularizers
+import tensorflow.keras.backend as K
+import tensorflow.keras as keras
+from tensorflow.keras.layers import Conv2D,Conv1D, MaxPooling1D,MaxPooling2D
+from tensorflow.keras.layers import Dropout,Flatten,Dense
+from tensorflow.keras import regularizers
 
 def add_conv_layer(model,n_kerns,kern_size,pool_size,
-                    input,activ='relu',one_dim=False):
-
+                    activ='relu',one_dim=False):
+    x=model
     Conv=Conv1D if(one_dim) else Conv2D
     MaxPooling=MaxPooling1D if(one_dim) else MaxPooling2D
     for i,n_kern_i in enumerate(n_kerns):
         print(i)
-        model.add(Conv(filters=n_kern_i, kernel_size=kern_size[i],activation=activ,name='conv%d'%i))
-        model.add(MaxPooling(pool_size=pool_size[i],name='pool%d' % i))
-    return model
+        kern_i,pool_i,name_i=kern_size[i],pool_size[i],'conv%d'%i
+        x=Conv(filters=n_kern_i,kernel_size=kern_i,activation=activ,name=name_i)(x)
+        x=MaxPooling(pool_size=pool_i,name='pool%d' % i)(x)
+    return x
 
 def full_layer(x,size=100,l1=0.01,dropout=0.5,activ='relu'):
     x=Flatten()(x)
--- ts_sim.py
+++ ts_sim.py
@@ -1,33 +1,9 @@
-#import tensorflow as tf
+import tensorflow as tf
 import tensorflow.keras.backend as K
 import tensorflow.keras as keras
-from keras.layers import Input,Dense,Flatten,Lambda
-from keras.models import Model,Sequential
-#import keras
-import data.seqs,deep,learn
-
-class TS(object):
-    def __init__(self, n_hidden=100,activ='relu',l1=0.01):
-        self.activ=activ
-        self.n_hidden=n_hidden
-        self.l1=l1
-        self.n_kerns=[32,32]
-        self.kern_size=[8,8]
-        self.pool_size=[2,2]
-        self.dropout=None
-
-    def __call__(self,params):
-        input_img=Input(shape=(params['dims']))
-        x=deep.add_conv_layer(input_img,self.n_kerns,self.kern_size,
-                            self.pool_size,activ=self.activ,one_dim=True)
-        x=deep.full_layer(x,size=self.n_hidden,l1=self.l1,
-            dropout=self.dropout,activ=self.activ)
-        x=Dense(units=params['n_cats'],activation='softmax')(x)
-        model = Model(input_img, x)
-        model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer='adam')
-        model.summary()
-        return model
+from tensorflow.keras.layers import Input,Dense,Flatten,Lambda
+from tensorflow.keras.models import Model
+import learn,data.seqs,deep
 
 class TS_SIM(object):
     def __init__(self,n_hidden=100,activ='relu'):
@@ -38,68 +14,44 @@ class TS_SIM(object):
         self.pool_size=[2,2]
 
     def __call__(self,params):
-        input_shape=params["input_shape"]
-        left_input = Input(input_shape)
-        right_input = Input(input_shape)
-
-       
-        model = Sequential()
-
-        encoded_l =self.build_model(model,params,input_shape) #model(left_input)
-        encoded_r =self.build_model(model,params,input_shape) #model(right_input)
-
-        prediction,loss=contr_loss(encoded_l,encoded_r)
-        siamese_net = Model(inputs=[left_input,right_input],outputs=prediction)
-    
-        optimizer = keras.optimizers.Adam(lr = 0.00006)
-        siamese_net.compile(loss=loss,optimizer=optimizer)
-        extractor=Model(inputs=model.get_input_at(0),outputs=model.get_layer("hidden").output)
-        extractor.summary()
-        return siamese_net,extractor
-
-    def build_model(self,model,params,input):
-        deep.add_conv_layer(model,self.n_kerns,self.kern_size,self.pool_size,
-                        input,self.activ)
-        model.add(Flatten())
-        model.add(Dense(self.n_hidden, activation=self.activ,
-            name='hidden',kernel_regularizer=None))
+        input_shape=params["input_shape"][1:]
+        img_a = Input(shape=input_shape)
+        img_b = Input(shape=input_shape)
+        feature_extractor = self.build_model(input_shape)
+        feats_a = feature_extractor(img_a)
+        feats_b = feature_extractor(img_b)
+        distance = Lambda(euclidean_distance)([feats_a, feats_b])
+        model = Model(inputs=[img_a, img_b], outputs=distance)
+        model.compile(loss=contrastive_loss, optimizer="adam")
+        feature_extractor.summary()
         return model
 
-def contr_loss(encoded_l,encoded_r):
-    raise Exception(type(encoded_l))
-    L2_layer = Lambda(euclidean_distance,output_shape=eucl_dist_output_shape)
-    return L2_layer([encoded_l, encoded_r]),contrastive_loss
+    def build_model(self,input_shape):
+        inputs = Input(input_shape)
+        x=deep.add_conv_layer(inputs,self.n_kerns,self.kern_size,
+                self.pool_size,activ=self.activ,one_dim=True)
+        x=Flatten()(x)
+        x=Dense(self.n_hidden, activation=self.activ,
+            name='hidden',kernel_regularizer=None)(x)
+        model = Model(inputs, x)
+        return model
 
-def contrastive_loss(y_true, y_pred):
-    margin = 50
-    square_pred = K.square(y_pred)
-    margin_square = K.square(K.maximum(margin - y_pred, 0))
-    return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
+def euclidean_distance(vectors):
+    (feats_a, feats_b) = vectors
+    sum_squared = K.sum(K.square(feats_a-feats_b), axis=1,keepdims=True)
+    return K.sqrt(K.maximum(sum_squared, K.epsilon()))
 
-def euclidean_distance(vects):
-    x, y = vects
-    sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
-    return K.sqrt(K.maximum(sum_square, K.epsilon()))
-
-def eucl_dist_output_shape(shapes):
-    shape1, shape2 = shapes
-    return (shape1[0], 1)
-
-def train_cnn(in_path,out_path,n_epochs=5):
-    make_nn=TS_NN()
-    read=data.seqs.read_seqs
-    train=learn.Train(to_dataset,make_nn,read,batch_size=16)
-    train(in_path,out_path,n_epochs)
+def contrastive_loss(y, preds, margin=1):
+    y = tf.cast(y, preds.dtype)
+    squaredPreds = K.square(preds)
+    squaredMargin = K.square(K.maximum(margin - preds, 0))
+    loss = K.mean(y * squaredPreds + (1 - y) * squaredMargin)
+    return loss
 
 def train_sim(in_path,out_path,n_epochs=5):
     make_nn=TS_SIM()
     read=data.seqs.read_seqs
     train=learn.SimTrain(read,make_nn)#,read,batch_size=16)
     train(in_path,out_path,n_epochs)
-
-def to_dataset(train):
-    X,y=train.to_dataset()
-    params={"dims":train.dims(),"n_cats":max(y)+1}	
-    return X,y,params
 
 train_sim("seqs","test")
