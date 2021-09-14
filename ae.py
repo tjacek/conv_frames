bba00e6d88c2227984596b2a7681c656ad28f6f3
tjacek <tjacek@agh.edu.pl>
2021-09-14 21:57:17 +0200

    get_train

--- ae.py
+++ ae.py
@@ -6,33 +6,36 @@ from tensorflow.keras.models import Model,Sequential
 from tensorflow.keras.layers import Input, Dense,Conv2D,Reshape,Conv2DTranspose
 from tensorflow.keras.layers import Flatten,MaxPooling2D,UpSampling2D
 from keras import regularizers
+from tensorflow.keras.models import load_model
 import tc_nn
-import data.imgs,data.seqs,learn
+import os.path
+import data.imgs,data.seqs,learn,files
 
 class Autoencoder(object):
-    def __init__(self,n_hidden=100):
+    def __init__(self,n_hidden=128):
         self.n_hidden=n_hidden
-        self.n_kerns=64
-        self.scale=(2,2)
+        self.n_kerns=[64,64,64]
+        self.scale=[(4,4),(2,2),(2,2)]
     
     def __call__(self,params):
         x,y=params["dims"]
+        n_layers=len(self.n_kerns)
         input_img = Input(shape=(x,y, params['n_channels']))
-        x = Conv2D(self.n_kerns, (5, 5), activation='relu',padding='same')(input_img)
-        x = MaxPooling2D(self.scale)(x)
-        x = Conv2D(16, (5, 5), activation='relu',padding='same')(x)
-        x = MaxPooling2D((2, 2))(x)
+        x=input_img   
+        for i in range(n_layers):
+            x = Conv2D(self.n_kerns[i], (5, 5), activation='relu',padding='same')(x)
+            x = MaxPooling2D(self.scale[i])(x)
         shape = K.int_shape(x)
         x=Flatten()(x)
-        encoded=Dense(100,name='hidden',kernel_regularizer=regularizers.l1(0.01))(x)    
+        reg=None#regularizers.l1(0.01)
+        encoded=Dense(self.n_hidden,name='hidden',kernel_regularizer=reg)(x)    
         x = Dense(shape[1]*shape[2]*shape[3])(encoded)
         x = Reshape((shape[1], shape[2], shape[3]))(x)
-        x = UpSampling2D((2, 2))(x)
-        x = Conv2DTranspose(16, (5, 5), activation='relu',padding='same')(x)
-        x = UpSampling2D(self.scale)(x)
-        x = Conv2DTranspose(self.n_kerns, (5, 5), activation='relu',padding='same')(x)
-    
-        x=Conv2DTranspose(filters=params['n_channels'],kernel_size=self.n_kerns,padding='same')(x)
+        for i in range(n_layers):
+            j=n_layers-i-1
+            x = UpSampling2D(self.scale[j])(x)
+            x = Conv2DTranspose(self.n_kerns[i], (5, 5), activation='relu',padding='same')(x) 
+        x=Conv2DTranspose(filters=params['n_channels'],kernel_size=self.n_kerns[0],padding='same')(x)
         recon=Model(input_img,encoded)
         autoencoder = Model(input_img, x)
 
@@ -41,24 +44,31 @@ class Autoencoder(object):
         autoencoder.summary()
         return autoencoder,recon
 
-def train_ae(in_path,out_path,n_batch=16):
-    read=tc_nn.SimpleRead(dim=(64,64),preproc=data.imgs.Downsample())
+def train_ae(in_path,out_path,n_batch=8,n_epochs=30):
+    read=tc_nn.SimpleRead(dim=(64,128),preproc=data.imgs.Downsample())
     seq_dict=read(in_path)
     train=seq_dict.split()[0]
     X,params=to_dataset(train,8)
-    make_ae=Autoencoder()
-    model=make_ae(params)[0]
-    model.fit(X,X,epochs=5,batch_size=n_batch)
+    model=get_train(params,out_path)
+    model.fit(X,X,epochs=n_epochs,batch_size=n_batch)
     model.save(out_path)
 
+def get_train(params,out_path):
+    if(os.path.isfile(out_path)):
+        model=load_model(out_path)
+    else:    
+        make_ae=Autoencoder()
+        model=make_ae(params)[0]
+    return model
+
 def extract_ae(in_path,nn_path,out_path):
-    read=tc_nn.SimpleRead(dim=(64,64),preproc=data.imgs.Downsample())
+    read=tc_nn.SimpleRead(dim=(64,128),preproc=data.imgs.Downsample())
     frame_dict=read(in_path)
     model=learn.base_read_model(frame_dict,nn_path)
     extractor=learn.get_extractor(model,"hidden")
     def helper(img_i):
         img_i=np.array(img_i)
-        img_i=img_i.astype('f8')        
+#        img_i=img_i.astype('f8')        
         feat_i=extractor.predict(img_i)
         return feat_i
     seq_dict=frame_dict.transform(helper,new=True,single=False)
@@ -76,8 +86,19 @@ def to_dataset(train,fraction=2):
     params={ 'n_channels':1,"dims":train.dims()}
     return X,params
 
+def test_read(in_path,out_path):
+    read=tc_nn.SimpleRead(dim=(64,128),preproc=data.imgs.Downsample())
+    seq_dict=read(in_path)
+    seq_dict.save(out_path)
 
+def ae_exp(frame_path,out_path,n_epochs=2):
+    files.make_dir(out_path)
+    model_path="%s/ae" % out_path
+    seq_path="%s/seqs" % out_path 
+    train_ae(frame_path,model_path,n_epochs=n_epochs)
+    extract_ae(frame_path,model_path,seq_path)
 
-in_path="../best2/frames"
-#train_ae(in_path,"test_ae")
-extract_ae(in_path,"test_ae","seqs")
+frame_path="../best2/frames"
+out_path="../best2/3_layers"
+ae_exp(frame_path,out_path,n_epochs=5)
+#test_read(frame_path,"frames")
