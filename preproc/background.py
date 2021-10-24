36451f9e36f0b5eae50c876c2b8182b1757d3880
tjacek <tjacek@agh.edu.pl>
2021-10-24 18:56:06 +0200

    background

--- preproc/background.py
+++ preproc/background.py
@@ -5,30 +5,44 @@ from skimage.measure import label
 import cv2 
 import data.actions,data.imgs
 
-def substract(in_path,action_path,out_path):
-    backgrounds=data.actions.read_actions(in_path,"color")
-    def helper(name_i,action_i):
+class Backgrounds(object):
+    def __init__(self,backgrounds):
+        self.backgrounds=backgrounds
+
+    def __call__(self,name_i,action_i):
         print(name_i)
         view_j=name_i.split("_")[-1].split(".")[0]
-        background_j=backgrounds[view_j]
+        background_j=self.backgrounds[view_j]
         diff_i=(action_i-background_j)
         diff_i[diff_i<0]=0
-        u, c = np.unique(diff_i, return_counts=True)
-        largest=c.argsort()[-20:]
-        for l in largest:
-            diff_i[diff_i==u[l]]=0
         return diff_i
+
+def substract(in_path,action_path,out_path):
+    backgrounds=data.actions.read_actions(in_path,"color")
+    backgrounds.transform(to_grey)
+    backgrounds=Backgrounds(backgrounds) 
+    def helper(name_i,action_i):
+        action_i=to_grey(action_i)
+        action_i=backgrounds(name_i,action_i)
+        action_i=remove_mc(action_i,k=20)
+        action_i=largest_cc(action_i)
+        return action_i
     data.actions.transform_lazy(action_path,out_path,helper)
 
-def larges_cc(in_path,out_path):
-    def helper(name_i,action_i):
-        segmentation=np.sum(action_i,axis=2)
-        segmentation[segmentation>0]=1
-        labels = label(segmentation)
-        largestCC = (labels == (np.argmax(np.bincount(labels.flat)[1:])+1))
-        segmentation[  largestCC]=100
-        return segmentation
-    data.actions.transform_lazy(in_path,out_path,helper)
+def remove_mc(diff_i,k=20):
+    u, c = np.unique(diff_i, return_counts=True)
+    largest=c.argsort()[-k:]
+    for l in largest:
+        diff_i[diff_i==u[l]]=0
+    return diff_i
+
+def largest_cc(segmentation):
+    segmentation[segmentation>0]=1
+    labels = label(segmentation)
+    largestCC = (labels == (np.argmax(np.bincount(labels.flat)[1:])+1))
+    segmentation[  largestCC]=100
+    segmentation[segmentation!=100]=0
+    return segmentation
 
 def cv_background(in_path,out_path,background_path):
     backgrounds=data.actions.read_actions(background_path,"color")
@@ -55,6 +69,6 @@ def to_grey(frames):
 in_path="../../Background/raw"
 action_path="../../actions"
 out_path="../../actions2"
-#substract(in_path,action_path,out_path)
+substract(in_path,action_path,out_path)
 #larges_cc(out_path,"../../actions3")
-cv_background("../../rgb","../../box",in_path)
+#cv_background("../../rgb","../../box",in_path)
